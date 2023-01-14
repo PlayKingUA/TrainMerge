22491c038d586c80dda57a8ce7636a9109fbb012
Vseslav <Vseslav2244@gmail.com>
2023-01-14 17:48:25 +0300

    cube removed

---
+++ Assets/_Scripts/Money Logic/MoneyWallet.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace _Scripts.Money_Logic
+{
+    public class MoneyWallet : MonoBehaviour
+    {
+        #region Variables
+        [SerializeField] private int moneyCount;
+        private const string SaveKey = "Money";
+
+        public int MoneyCount => moneyCount;
+        
+        public event Action<int> MoneyCountChanged;
+        #endregion
+        
+        #region Monobehaviour Callbacks
+        private void Start()
+        {
+            Load();
+        }
+        #endregion
+
+        #region Add/Get
+        public void Add(int addAmount)
+        {
+            moneyCount += addAmount;
+            MoneyCountChanged?.Invoke(moneyCount);
+            
+            Save();
+        }
+
+        public void Get(int getAmount)
+        {
+            moneyCount -= getAmount;
+            MoneyCountChanged?.Invoke(moneyCount);
+
+            Save();
+        }
+        #endregion
+        
+        #region Save/Load
+        private void Save()
+        {
+            PlayerPrefs.SetInt(SaveKey, moneyCount);
+        }
+
+        private void Load()
+        {
+            moneyCount = PlayerPrefs.GetInt(SaveKey, moneyCount);
+            
+            MoneyCountChanged?.Invoke(moneyCount);
+        }
+        #endregion
+    }
+}
